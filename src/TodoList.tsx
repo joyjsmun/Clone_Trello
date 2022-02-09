1b052b2da528fad5a51b176bfc6a153508b3eb8e
joyjsmun <joy.js.mun@gmail.com>
2022-02-08 18:19:41 -0800

    ADD: react-hook-form

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,9 +1,38 @@
-function ToDoList(){
+import React, { useState } from "react";
+import {useForm} from "react-hook-form";
+
+/* function ToDoList(){
+    const [toDo,setToDo] = useState("");
+    const onChange = (event:React.FormEvent<HTMLInputElement>) => {
+        const {
+            currentTarget:{value},
+        } = event;
+        setToDo(value);
+    };
+    const onSubmit = (event:React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        console.log(toDo);
+    };
     return (
-        <div>
-            hello
-        </div>
+       <div>
+           <form onSubmit={onSubmit}>
+           <input onChange={onChange} value={toDo} placeholder="What is your todo" />
+            <button>Add</button>
+            </form>
+       </div>
     );
 } 
+ */
+
+function ToDoList(){
+    const {register} = useForm();
+    console.log(register("toDo"))
+    return <div>
+        <form>
+            <input placeholder="Write a to do" />
+            <button>Add</button>
+        </form>
+    </div>
+}
 
 export default ToDoList;
