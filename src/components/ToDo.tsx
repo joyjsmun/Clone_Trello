3e3ae6cc4eb27dd29d038c9ac0ad4cc2b514ab5e
joyjsmun <joy.js.mun@gmail.com>
2022-02-09 14:59:56 -0800

    ADD: button showing by category type

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,12 +1,20 @@
-import { ITodo } from "../atoms";
+import React from "react";
+import { ITodo, toDoState } from "../atoms";
+import {useSetRecoilState} from "recoil";
 
-function ToDo({text}:ITodo){
+function ToDo({text,category,id}:ITodo){
+    const setToDos = useSetRecoilState(toDoState);
+   const onClick = (event:React.MouseEvent<HTMLButtonElement>) => {
+    const {
+        currentTarget:{name}, 
+    } = event ;
+   }
     return (
         <li>
             <span>{text}</span>;
-            <button>To Do</button>
-            <button>Doing</button>
-            <button>Done</button>
+            {category !== "To_Do" && <button name="To_Do" onClick={onClick}>To Do</button>}
+            {category !== "Doing" && <button name="Doing" onClick={onClick}>Doing</button>}
+            {category !== "Done" && <button name="Done" onClick={onClick}>Done</button>}
         </li>
     )
 }
