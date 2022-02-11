3f6b13238cb790ec6e66b4e8013a75ccd177be3d
joyjsmun <joy.js.mun@gmail.com>
2022-02-11 11:05:40 -0800

    ADD: styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,62 @@
 import {DragDropContext,Droppable,Draggable} from "react-beautiful-dnd";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  max-width: 480px ;
+  width: 100%;
+  margin: 0 auto;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`
+
+const Boards = styled.div`
+  display: grid;
+  width: 100%;
+  grid-template-columns:repeat(1,1fr) ;
+  min-height: 200px;
+`
+
+
+const Board = styled.div`
+  padding:0px 10px;
+  padding-top: 30px;
+  background-color: ${(props) => props.theme.boardColor};
+  border-radius:3px;
+  
+`
+
+const Card = styled.div`
+  border-radius:3px;
+  padding:10px 10px;
+  margin-bottom: 5px;
+  background-color: ${(props) => props.theme.cardColor};
+`
+
+const toDos = ["a","b","c","d","e"]
 
 function App(){
   const onDragEnd = () => {}
   return <DragDropContext onDragEnd={onDragEnd}>
-    <div>
-      <Droppable droppableId="one">
+    <Wrapper>
+      <Boards>
+     <Droppable droppableId="one">
         {(magic) => 
-        <ul ref={magic.innerRef} {...magic.droppableProps}>
-          <Draggable draggableId="first" index={0}>
+        <Board ref={magic.innerRef} {...magic.droppableProps}>
+          {toDos.map((toDo,index) =><Draggable draggableId={toDo} index={index}>
             {(magic) => 
-              <li ref={magic.innerRef} 
-               {...magic.draggableProps}>
-                 <span {...magic.dragHandleProps}>🐯</span>
-                 Joy
-               </li>}
-            </Draggable>  
-            <Draggable draggableId="second" index={1}>
-            {(magic) => 
-              <li ref={magic.innerRef}{...magic.draggableProps}>
-                <span  {...magic.dragHandleProps} >❤️</span>
-                Love</li>}
-            </Draggable>    
-            <Draggable draggableId="Third" index={2}>
-            {(magic) => 
-              <li ref={magic.innerRef}{...magic.draggableProps}>
-                <span  {...magic.dragHandleProps} >😝</span>
-                You</li>}
-            </Draggable>  
-          </ul>}
+              <Card ref={magic.innerRef} 
+               {...magic.draggableProps} 
+               {...magic.dragHandleProps}>
+                 {toDo}
+               </Card>}
+            </Draggable>)}   
+          {magic.placeholder}
+          </Board>}
       </Droppable>
-    </div>
+      </Boards>
+    </Wrapper>
   </DragDropContext>;
 }
 
--- src/theme.ts
+++ src/theme.ts
@@ -1,8 +1,7 @@
 import { DefaultTheme } from "styled-components";
 
 export const darkTheme: DefaultTheme = {
-  bgColor: "#2f3640",
-  textColor: "white",
-  accentColor: "#9c88ff",
-  cardBgColor: "transparent",
+  bgColor: "#3f8cf2",
+  boardColor:"#b4e0ff",
+  cardColor:"white",
 };
