8792d91f3356c16155f2f0f33ea924d71c51319e
joyjsmun <joy.js.mun@gmail.com>
2022-02-11 10:06:56 -0800

    ADD: practice selector

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,17 @@ import { hoursSelector, muinuteState } from "./atoms";
 
 function App() {
   const [mins,setMins] = useRecoilState(muinuteState);
-  const hours = useRecoilValue(hoursSelector);
+  const [hours,setHours] = useRecoilState(hoursSelector);
   const onMinsChange = (event:React.FormEvent<HTMLInputElement>) =>{
     setMins(+event.currentTarget.value)
+  }
+  const onHoursChange = (event:React.FormEvent<HTMLInputElement>) =>{
+    setHours(+event.currentTarget.value)
   }
   return (
    <div>
      <input value={mins} onChange={onMinsChange} type="number" placeholder="Minutes" />
-     <input value={hours} type="number" placeholder="Hours"  />
+     <input value={hours} onChange={onHoursChange} type="number" placeholder="Hours"  />
 
    </div>
   );
--- src/atoms.tsx
+++ src/atoms.tsx
@@ -3,15 +3,18 @@ import {atom,selector} from "recoil";
 
 export const muinuteState = atom({
     key:"minutes",
-    default:0,
+    default: 0,
 })
 
 
-export const hoursSelector = selector({
+export const hoursSelector = selector<number>({
     key:"hours",
     get:({get}) =>{
         const mins = get(muinuteState);
         return mins/60;
-    }
-
+    },
+    set:({set},newValue) => {
+        const mins = Number(newValue) * 60;
+        set(muinuteState,mins);
+    },
 })
